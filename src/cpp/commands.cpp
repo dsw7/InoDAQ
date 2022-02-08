b466ca0a3ac250631a540cd4925aa3ae3d077028
David Weber <dsw7@sfu.ca>
2022-02-08 07:47:18 +0000

    Add new test command

--- src/cpp/commands.cpp
+++ src/cpp/commands.cpp
@@ -25,7 +25,7 @@ int run_ping_command(std::string &serial_port, const bool &is_verbose)
         connection.read_data();
     }
 
-    sleep(1);
+    usleep(250000);
 
     info("Built in LED should now turn off", true);
     if (connection.write_data("test\n"))
@@ -34,5 +34,58 @@ int run_ping_command(std::string &serial_port, const bool &is_verbose)
     }
 
     connection.close_connection();
+    return EXIT_SUCCESS;
+}
+
+int run_test_command(std::string &serial_port, const bool &is_verbose)
+{
+    info("Running test command", true);
+
+    static std::vector<std::string> digital_pins = {
+        "D2\n", "D3\n", "D4\n", "D5\n",
+        "D6\n", "D7\n", "D8\n", "D9\n",
+        "D10\n", "D11\n", "D12\n", "D13\n"
+    };
+
+    Serial connection(is_verbose);
+
+    if (not connection.open_connection(serial_port))
+    {
+        return EXIT_FAILURE;
+    }
+
+    if (not connection.configure_connection())
+    {
+        connection.close_connection();
+        return EXIT_FAILURE;
+    }
+
+    sleep(3);
+
+    info("Turning digital pins on", true);
+
+    for (std::vector<std::string>::iterator p = digital_pins.begin(); p != digital_pins.end(); ++p)
+    {
+        usleep(100000);
+
+        if (connection.write_data(*p))
+        {
+            connection.read_data();
+        }
+    }
+
+    info("Turning digital pins off", true);
+    sleep(1);
+
+    for (std::vector<std::string>::iterator p = digital_pins.begin(); p != digital_pins.end(); ++p)
+    {
+        usleep(100000);
+
+        if (connection.write_data(*p))
+        {
+            connection.read_data();
+        }
+    }
+
     return EXIT_SUCCESS;
 }
--- src/cpp/commands.h
+++ src/cpp/commands.h
@@ -1,9 +1,13 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <vector>
+#include <unistd.h>
+
 #include "serial.h"
 #include "logging.h"
 
 int run_ping_command(std::string &port, const bool &is_verbose);
+int run_test_command(std::string &port, const bool &is_verbose);
 
 #endif
