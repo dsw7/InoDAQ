6920d5de8f930eb582bdb8fc9bf0386fc5624f6d
David Weber <dsw7@sfu.ca>
2022-02-09 06:45:11 +0000

    Fix test

--- test/test_serial.py
+++ test/test_serial.py
@@ -8,7 +8,7 @@ CONNECTION_KWARGS = {
     'parity': serial.PARITY_NONE,
     'stopbits': serial.STOPBITS_ONE,
     'bytesize': serial.EIGHTBITS,
-    'timeout': 1
+    'timeout': 5
 }
 
 
@@ -34,7 +34,7 @@ class TestInoDAQ:
             if not retval:
                 continue
 
-            if retval[0] == b'foobar\r\n':
+            if b'foobar' in retval[0]:
                 break
         else:
             pytest.exit(f'Could not connect to {self.handle.name} in time')
@@ -47,24 +47,24 @@ class TestInoDAQ:
     @pytest.mark.parametrize(
         'command, on_msg, off_msg',
         [
-            (b'D2\n',  b'2: ON\r\n',  b'2: OFF\r\n'),
-            (b'D3\n',  b'3: ON\r\n',  b'3: OFF\r\n'),
-            (b'D4\n',  b'4: ON\r\n',  b'4: OFF\r\n'),
-            (b'D5\n',  b'5: ON\r\n',  b'5: OFF\r\n'),
-            (b'D6\n',  b'6: ON\r\n',  b'6: OFF\r\n'),
-            (b'D7\n',  b'7: ON\r\n',  b'7: OFF\r\n'),
-            (b'D8\n',  b'8: ON\r\n',  b'8: OFF\r\n'),
-            (b'D9\n',  b'9: ON\r\n',  b'9: OFF\r\n'),
-            (b'D10\n', b'10: ON\r\n', b'10: OFF\r\n'),
-            (b'D11\n', b'11: ON\r\n', b'11: OFF\r\n'),
-            (b'D12\n', b'12: ON\r\n', b'12: OFF\r\n'),
-            (b'D13\n', b'13: ON\r\n', b'13: OFF\r\n')
+            (b'D2\n',  b'2: ON',  b'2: OFF'),
+            (b'D3\n',  b'3: ON',  b'3: OFF'),
+            (b'D4\n',  b'4: ON',  b'4: OFF'),
+            (b'D5\n',  b'5: ON',  b'5: OFF'),
+            (b'D6\n',  b'6: ON',  b'6: OFF'),
+            (b'D7\n',  b'7: ON',  b'7: OFF'),
+            (b'D8\n',  b'8: ON',  b'8: OFF'),
+            (b'D9\n',  b'9: ON',  b'9: OFF'),
+            (b'D10\n', b'10: ON', b'10: OFF'),
+            (b'D11\n', b'11: ON', b'11: OFF'),
+            (b'D12\n', b'12: ON', b'12: OFF'),
+            (b'D13\n', b'13: ON', b'13: OFF')
         ]
     )
     def test_digital_pins(self, command: bytes, on_msg: bytes, off_msg: bytes) -> None:
 
         self.handle.write(command)
-        assert self.handle.readlines()[0] == on_msg
+        assert on_msg in self.handle.readlines()[0]
 
         self.handle.write(command)
-        assert self.handle.readlines()[0] == off_msg
+        assert off_msg in self.handle.readlines()[0]
