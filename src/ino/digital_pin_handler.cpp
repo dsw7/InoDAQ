804bbaa81426dbe02eb2bdebaca74461fe1c87fa
David Weber <dsw7@sfu.ca>
2022-02-09 05:39:56 +0000

    snprintf -> sprintf

--- src/ino/digital_pin_handler.cpp
+++ src/ino/digital_pin_handler.cpp
@@ -26,17 +26,18 @@ void set_digital_pin_status(int &pin)
     // 9 bytes to handle strcat of "13" + ": OFF"
     // 1 3 \0 : _ 0 F F \0
     // 1 2 3  4 5 6 7 8 9
-    char pin_c[9];
-    snprintf(pin_c, 9, "%d", pin);
+    char message[10]; // add space for one extra byte just in case
+
+    sprintf(message, "%d", pin);
 
     if (pins[idx])
     {
-        strcat(pin_c, ": ON");
-        send_message(pin_c);
+        strcat(message, ": ON");
+        send_message(message);
         return;
     }
 
-    strcat(pin_c, ": OFF");
-    send_message(pin_c);
+    strcat(message, ": OFF");
+    send_message(message);
 }
 
