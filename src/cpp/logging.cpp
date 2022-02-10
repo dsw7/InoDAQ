79bff1aed9097608a3da88d9e16b61364405c35d
David Weber <dsw7@sfu.ca>
2022-02-10 08:27:48 +0000

    Add progress bar function

--- src/cpp/logging.cpp
+++ src/cpp/logging.cpp
@@ -20,3 +20,28 @@ void error(const std::string &message)
 {
     std::cerr << "\033[31m[ E ]\033[0m " << message << std::endl;
 }
+
+void render_progress_bar(int delay_secs)
+{
+
+    int step_size = delay_secs * pow(10, 6) / 20;
+
+    std::string progress_bar = "[                    ]";
+
+    for (unsigned int i = 1; i < 21; i++)
+    {
+        if (i == 1)
+        {
+            progress_bar.at(i) = '>';
+        }
+        else
+        {
+            progress_bar.at(i - 1) = '-';
+            progress_bar.at(i) = '>';
+        }
+        usleep(step_size);
+        std::cout << progress_bar + "\r" << std::flush;
+    }
+
+    std::cout << std::endl;
+}
--- src/cpp/logging.h
+++ src/cpp/logging.h
@@ -3,9 +3,12 @@
 
 #include <string.h>
 #include <iostream>
+#include <math.h>
+#include <unistd.h>
 
 void info(const std::string &message, const bool &verbose);
 void warning(const std::string &message, const bool &verbose);
 void error(const std::string &message);
+void render_progress_bar(int delay_secs);
 
 #endif
