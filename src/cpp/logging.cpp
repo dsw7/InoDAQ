75c53d55147667a396b5570f8bb9696efaa76449
David Weber <dsw7@sfu.ca>
2022-02-21 23:08:40 -0800

    Replace verbose with quiet arg in logging + remove progress bar

--- src/cpp/logging.cpp
+++ src/cpp/logging.cpp
@@ -1,50 +1,28 @@
 #include "logging.h"
 
-void info(const std::string &message, const bool &verbose)
+void info(const std::string &message, const bool &is_quiet)
 {
-    if (verbose)
+    if (is_quiet)
     {
-        std::cout << "\033[32m[ I ]\033[0m " << message << std::endl;
+        return;
     }
+    std::cout << "\033[32m[ I ]\033[0m " << message << std::endl;
 }
 
-void warning(const std::string &message, const bool &verbose)
+void warning(const std::string &message, const bool &is_quiet)
 {
-    if (verbose)
+    if (is_quiet)
     {
-        std::cout << "\033[33m[ W ]\033[0m " << message << std::endl;
+        return;
     }
+    std::cout << "\033[33m[ W ]\033[0m " << message << std::endl;
 }
 
-void error(const std::string &message, const bool &verbose)
+void error(const std::string &message, const bool &is_quiet)
 {
-    if (verbose)
+    if (is_quiet)
     {
-        std::cerr << "\033[31m[ E ]\033[0m " << message << std::endl;
+        return;
     }
-}
-
-void render_progress_bar(int delay_secs)
-{
-
-    int step_size = delay_secs * pow(10, 6) / 20;
-
-    std::string progress_bar = "[                    ]";
-
-    for (unsigned int i = 1; i < 21; i++)
-    {
-        if (i == 1)
-        {
-            progress_bar.at(i) = '>';
-        }
-        else
-        {
-            progress_bar.at(i - 1) = '-';
-            progress_bar.at(i) = '>';
-        }
-        usleep(step_size);
-        std::cout << progress_bar + "\r" << std::flush;
-    }
-
-    std::cout << std::endl;
+    std::cerr << "\033[31m[ E ]\033[0m " << message << std::endl;
 }
--- src/cpp/logging.h
+++ src/cpp/logging.h
@@ -6,9 +6,9 @@
 #include <math.h>
 #include <unistd.h>
 
-void info(const std::string &message, const bool &verbose);
-void warning(const std::string &message, const bool &verbose);
-void error(const std::string &message, const bool &verbose);
+void info(const std::string &message, const bool &is_quiet);
+void warning(const std::string &message, const bool &is_quiet);
+void error(const std::string &message, const bool &is_quiet);
 void render_progress_bar(int delay_secs);
 
 #endif
