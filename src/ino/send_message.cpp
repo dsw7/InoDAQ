bdd6e3b81968ad1225ccd14851256f197cb79c1b
David Weber <dsw7@sfu.ca>
2022-02-24 00:47:44 -0800

    Test

--- src/ino/send_message.cpp
+++ src/ino/send_message.cpp
@@ -6,11 +6,12 @@ void send_message(char *message)
 
     char padding[size_padding + 5];
 
-    for (unsigned int i = 0; i <= size_padding; i++)
+    for (unsigned int i = 0; i < size_padding; i++)
     {
         padding[i] = 'x';
     }
 
+    padding[size_padding] = '\n';
     padding[size_padding + 1] = '\0';
 
     char payload[MAX_SIZE_PAYLOAD];
@@ -21,5 +22,5 @@ void send_message(char *message)
     // Use Serial.print() because Serial.println() adds two additional bytes, CRLF, to the outgoing
     // payload which is a problem when reading the message on the receiver's end
     Serial.print(payload);
+    //Serial.flush();
 }
-
