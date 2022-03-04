be5267e8a973d329ae0be4f18fa5f7f5a5359e47
David Weber <dsw7@sfu.ca>
2022-03-04 00:08:19 -0800

    Add basic digital pin toggler template

--- src/ino/src/commands.cpp
+++ src/ino/src/commands.cpp
@@ -5,6 +5,7 @@ namespace Protocol
     const char* MESSAGE_FIN = "FIN";
     const char* MESSAGE_FIN_ACK = "FIN-ACK";
     const char* MESSAGE_TOGGLE_BUILTIN_LED = "TEST";
+    const char* MESSAGE_PREFIX_TOGGLE_DIG_PIN = "DIG";
 }
 
 void toggle_builtin_led(char *buffer)
@@ -23,6 +24,31 @@ void toggle_builtin_led(char *buffer)
     strcpy(buffer, "Built in LED is OFF");
 }
 
+void toggle_digital_pin(char* buffer_input, char* buffer_output)
+{
+    if (strcmp(buffer_input, Protocol::MESSAGE_PREFIX_TOGGLE_DIG_PIN) == 0)
+    {
+        strcpy(buffer_output, Protocol::MESSAGE_PREFIX_TOGGLE_DIG_PIN);
+        return;
+    }
+
+    char substr_pin[3];
+
+    strncpy(substr_pin, &buffer_input[3], 2);
+    substr_pin[2] = '\0';
+
+    int pin = atoi(substr_pin);
+
+    if ((pin < 2) or (pin > 13))
+    {
+        strcpy(buffer_output, buffer_input);
+        return;
+    }
+
+    // TODO continue here
+    strcpy(buffer_output, "ABC");
+}
+
 bool parser(char* buffer_input, char* buffer_output)
 {
 
@@ -35,6 +61,12 @@ bool parser(char* buffer_input, char* buffer_output)
     if (strcmp(buffer_input, Protocol::MESSAGE_TOGGLE_BUILTIN_LED) == 0)
     {
         toggle_builtin_led(buffer_output);
+        return true;
+    }
+
+    if (strncmp(buffer_input, Protocol::MESSAGE_PREFIX_TOGGLE_DIG_PIN, sizeof(Protocol::MESSAGE_PREFIX_TOGGLE_DIG_PIN)) == 0)
+    {
+        toggle_digital_pin(buffer_input, buffer_output);
         return true;
     }
 
--- src/ino/src/commands.h
+++ src/ino/src/commands.h
@@ -5,6 +5,7 @@
 #include <string.h>
 
 void toggle_builtin_led(char *buffer);
+void toggle_digital_pin(char* buffer_input, char* buffer_output);
 bool parser(char* buffer_input, char* buffer_output);
 
 #endif
