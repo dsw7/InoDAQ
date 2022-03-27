a024cde47e897e164363fedf49def4467eb39da7
David Weber <dsw7@sfu.ca>
2022-03-26 22:01:31 -0700

    Shorten test

--- src/cpp/tests/test_presentation_layer.py
+++ src/cpp/tests/test_presentation_layer.py
@@ -4,22 +4,14 @@ from subprocess import run
 
 def test_help_menu_long(pytestconfig):
     command = [pytestconfig.getoption('binary'), '--help']
-
     logging.debug('Running command: "%s"', ' '.join(command))
+
     process = run(command, capture_output=True, check=True)
-
-    logging.debug('The stdout was:\n%s', process.stdout.decode())
-    logging.debug('The stderr was:\n%s', process.stderr.decode())
-
     assert process.returncode == EX_OK
 
 def test_help_menu_short(pytestconfig):
     command = [pytestconfig.getoption('binary'), '-h']
-
     logging.debug('Running command: "%s"', ' '.join(command))
+
     process = run(command, capture_output=True, check=True)
-
-    logging.debug('The stdout was:\n%s', process.stdout.decode())
-    logging.debug('The stderr was:\n%s', process.stderr.decode())
-
     assert process.returncode == EX_OK
